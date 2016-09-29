6200937c9dc21e08582aa625ae12349a5be26dc4
Juan Caicedo <juan@spanishdict.com>
2016-09-28 21:11:50 -0500

    get issues by name and prompt user

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 #! /usr/bin/env node
 
 const GitHubApi = require('github');
-const program = require('commander');
+const commander = require('commander');
+const inquirer = require('inquirer');
 
-program
-  .option('-r, --repo <mine>', 'A github repository')
-  .parse(process.argv);
+function getCommandLineInputs(args) {
+  return commander
+    .option('-r, --repo <mine>', 'A github repository')
+    .parse(args);
+}
 
-const split = program.repo.split('/');
-const user = split[0];
-const repo = split[1];
+function getRepoIfMissing(program) {
+  if (!program.repo) {
+    return inquirer.prompt([{
+        name: 'repo',
+        message: 'What repo are you interested in?'
+      }])
+      .then(function(answers) {
+        program.repo = answers.repo;
+        return program;
+      });
+  };
+  return program;
+}
 
-const github = new GitHubApi();
-github.issues.getForRepo({
-  user: user,
-  repo: repo
-}, function(err, res) {
-  console.log(err, res);
-});
+function parseUserAndRepoName(program) {
+  const split = program.repo.split('/');
+  const user = split[0];
+  const repoName = split[1];
+
+  program.user = user;
+  program.repoName = repoName;
+  return program;
+}
+
+function getIssues(program) {
+  const github = new GitHubApi();
+  return github.issues.getForRepo({
+    user: program.user,
+    repo: program.repoName
+  });
+}
+
+function getTitles(issues) {
+  return issues.map(issue => issue.title);
+}
+
+function main() {
+  Promise.resolve(getCommandLineInputs(process.argv))
+    .then(getRepoIfMissing)
+    .then(parseUserAndRepoName)
+    .then(getIssues)
+    .then(getTitles)
+    .then(console.log);
+}
+
+if (require.main === module) {
+  main();
+}
