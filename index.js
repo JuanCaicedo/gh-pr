54381932e9764b1a9c6dd22a03baa9b2684e2440
Juan Caicedo <juan@spanishdict.com>
2016-09-28 20:30:44 -0500

    log repo issues

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
 
-console.log('hello world');
+const GitHubApi = require('github');
+const program = require('commander');
+
+program
+  .option('-r, --repo <mine>', 'A github repository')
+  .parse(process.argv);
+
+const split = program.repo.split('/');
+const user = split[0];
+const repo = split[1];
+
+const github = new GitHubApi();
+github.issues.getForRepo({
+  user: user,
+  repo: repo
+}, function(err, res) {
+  console.log(err, res);
+});
